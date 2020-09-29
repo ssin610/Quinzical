dce8b6b1fe14f515dec1dce485a1047736de4918
ssin610 <ssin610@aucklanduni.ac.nz>
2020-09-29 15:29:48 +1300

    Game Mode || Implemented randomly selecting and displaying categories and clues

---
+++ Quinzical/src/application/TextFileReader.java
@@ -0,0 +1,27 @@
+package application;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+// helper class to read from files when starting game
+public class TextFileReader {
+
+	public List<String> read(File file) {
+		List<String> lines = new ArrayList<String>();
+		String line;
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			while ((line = br.readLine()) != null) {
+				lines.add(line);
+			}
+			br.close();
+		} catch (IOException ex) {
+			
+		}
+		return lines;
+	}
+}
--- Quinzical/src/application/questionBoardController.java
+++ Quinzical/src/application/questionBoardController.java
@@ -3,8 +3,12 @@ package application;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.ResourceBundle;
+import java.util.concurrent.ThreadLocalRandom;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -38,6 +42,10 @@ public class questionBoardController implements Initializable {
     int index_y = 0;
     int index_x = 0;
 
+    ArrayList<File> addedCategories = new ArrayList<File>();
+
+    
+
     public void initialize(URL url, ResourceBundle rb) {
         resetText.setVisible(false);
         reset.setVisible(false);
@@ -45,21 +53,56 @@ public class questionBoardController implements Initializable {
         File dir = new File("cat"); // get location of categories folder
         File[] categoryFolder = dir.listFiles();
         if (categoryFolder != null) {
-            for (File category : categoryFolder) { // iterate through each category
-
+            for (int i = 0; i < 5; i++) { // iterate through each category
+                ArrayList<Integer> addedIndex = new ArrayList<Integer>();
+                File randomCat = getRandom(categoryFolder);
+                while (addedCategories.contains(randomCat)) {
+                    randomCat = getRandom(categoryFolder);
+                }
+                addedCategories.add(randomCat);
                 index_y = 0;
-                Text categoryName = new Text(category.getName().toUpperCase());
+                Text categoryName = new Text(randomCat.getName().toUpperCase());
                 categoryName.setFont(Font.font("Agency FB", 20));
                 categoryName.setFill(Color.LIGHTSKYBLUE);
                 grid.add(categoryName, index_x, index_y);
                 GridPane.setHalignment(categoryName, HPos.CENTER);
+
+                TextFileReader reader = new TextFileReader();
+                List<String> lines = reader.read(randomCat);
+                for (int j = 0; j < 5; j++) { // go through every line of the category file and parse the results,
+                                              // forming
+                                              // the respective fields
+                    int randomIndex = ThreadLocalRandom.current().nextInt(0, lines.size());
+                    while (addedIndex.contains(randomIndex)) {
+                        randomIndex = ThreadLocalRandom.current().nextInt(0, lines.size());
+                    }
+                    System.out.println(lines.get(randomIndex) + " | ");
+                    String value = lines.get(randomIndex).split("\\,")[2];
+                    addedIndex.add(randomIndex);
+                    index_y++;
+                    addButton(value); // add it to the board
+
+                }
                 index_x++;
             }
         } else {
             // the case when the category folder is not found
             System.out.println("Category folder not found");
         }
+    }
 
-	}
-	
+    public static File getRandom(File[] array) {
+        int rnd = new Random().nextInt(array.length);
+        return array[rnd];
+    }
+
+    public void addButton(String text) {
+        Button button = new Button("$" + text);
+        button.setPrefSize(190, 80);
+        button.setFont(Font.font("Agency FB", 20));
+        button.setStyle("-fx-background-color: #ffc100; ");
+        grid.add(button, index_x, index_y);
+        GridPane.setHalignment(button, HPos.CENTER);
+    }
+
 }
